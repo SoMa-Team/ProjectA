494aa1127b30fd68caf9d0ea1ebee8a6d9c22ef5
SooinPark1019 <hyperion1019@naver.com>
2025-05-02 04:27:00 +0900

    feature : 캐릭터 방향 전환 추가

--- Assets/Undead Survivor/Player.cs
+++ Assets/Undead Survivor/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffb4e15f2c0af21daca15bec9bc2845adf52bccd06bd68cb84699b4daf7b932e
-size 587
+oid sha256:b47afe1a087d1f7b3be7c9c7b6c2f3c3f4601a4ec78d051009226a05a7c01a11
+size 807
